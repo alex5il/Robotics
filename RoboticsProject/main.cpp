3e6bcea0fd4783db088554f9a72bb8713b0ddfed
alex5il <alexkono91@gmail.com>
2015-06-26 17:17:52 +0300

    Main

--- RoboticsProject/main.cpp
+++ RoboticsProject/main.cpp
@@ -7,22 +7,22 @@
 int main() {
 	//Robot robot("localhost",6665);
 
-//	Map* map = new Map();
-//
-//	map->createGridFromImage(map->originalImage);
-//	map->printMap();
-//
-//	std::cout << "\n\n\n ============================================== \n\n\n";
-//	std::cout << "\n\n\n ============================================== \n\n\n";
-//	std::cout << "\n\n\n ============================================== \n\n\n";
-//	std::cout << "\n\n\n ============================================== \n\n\n";
-//
-//	map->inflateImage();
-//	map->createGridFromImage(map->inflatedImage);
-//	map->printMap();
-//
-//
-//	map->createGridWithResolutionFromImage(map->inflatedImage);
+	Map* map = new Map();
+
+	map->createGridFromImage(map->originalImage);
+	map->printMap();
+
+	std::cout << "\n\n\n ============================================== \n\n\n";
+	std::cout << "\n\n\n ============================================== \n\n\n";
+	std::cout << "\n\n\n ============================================== \n\n\n";
+	std::cout << "\n\n\n ============================================== \n\n\n";
+
+	map->inflateImage();
+	map->createGridFromImage(map->inflatedImage);
+	map->printMap();
+
+
+	map->createGridWithResolutionFromImage(map->inflatedImage);
 
 //PlnObstacleAvoid plnOA(&robot);
 //Manager manager(&robot, &plnOA);
